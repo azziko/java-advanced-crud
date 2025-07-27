be1cbb42ba50dc88a859ca3b6435bed154ee09b7
Aziz <sharipov.wdev@gmail.com>
2025-07-27 12:35:37 +0200

    feat: update webrouter to handle admin endpoints

--- src/main/java/cz/sharipov/azul/crud/router/WebRouter.java
+++ src/main/java/cz/sharipov/azul/crud/router/WebRouter.java
@@ -1,5 +1,6 @@
 package cz.sharipov.azul.crud.router;
 
+import cz.sharipov.azul.crud.model.Poem;
 import cz.sharipov.azul.crud.service.PoemService;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,6 +29,34 @@ public class WebRouter {
     @GetMapping("/admin")
     public String adminHome(Model model) {
         model.addAttribute("poems", poemService.searchPoemsByContent("", 0, 100, null).getContent());
+        model.addAttribute("poem", new Poem());
         return "admin";
+    }
+
+    @GetMapping("/admin/poem/{id}")
+    public String adminEditing(@PathVariable("id") long id, Model model) {
+        model.addAttribute("poem", poemService.getPoem(id));
+        return "edit";
+    }
+
+    @PostMapping("/admin/poem/{id}")
+    public String adminEdit(@PathVariable("id") long id, @ModelAttribute Poem poem, Model model) {
+        model.addAttribute("poem", poemService.updatePoem(id, poemService.updatePoem(id, poem)));
+        return "redirect:/admin";
+    }
+
+    @PostMapping("/admin/poems")
+    public String adminAdd(@ModelAttribute Poem poem) {
+        poemService.savePoem(poem);
+        return "redirect:/admin";
+    }
+
+    @PostMapping("/admin/poems/{id}")
+    public String adminDelete(@PathVariable Long id, @RequestParam(value = "method", required = false) String method) {
+        if ("delete".equalsIgnoreCase(method)) {
+            poemService.deletePoem(id);
+        }
+        
+        return "redirect:/admin";
     }
 }
